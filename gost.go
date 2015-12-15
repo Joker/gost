dbb1ce1551ad9cc93335bc923eba6f0b76b39d14
Joker <nice.way@live.com>
2015-12-15 14:49:12 +0300

    all in list

--- gost.go
+++ gost.go
@@ -16,38 +16,53 @@ type workDir 	struct {
 }	
 
 
-func (dot *workDir) getSubdirectory() {
-	wd, err := os.Getwd()
-	if err != nil {
-		log.Println("Fail Getwd() - %s", err)
-		os.Exit(2)
-	}
-	dot.dirs = append(dot.dirs, wd)
-
-	fileInfo, err := ioutil.ReadDir(wd)
-	if err != nil {
-		log.Println("Fail ReadDir() - %s", err)
-		os.Exit(2)
-	}
-
-	var fname string
-	for _, file := range fileInfo {
-		fname = file.Name()
-
-		if filepath.Ext(fname) == ".yml" {
-			dot.goFiles = append(dot.goFiles, fname)
-			continue
+func (dot *workDir) parseDir(sl int) {
+	for _, wd := range dot.dirs[sl:] {
+		fileInfo, err := ioutil.ReadDir(wd)
+		if err != nil {
+			log.Println("Fail ReadDir() - %s", err)
+			os.Exit(2)
 		}
 
-		if file.IsDir() == true && fname[0] != '.' {
-			dot.dirs = append(dot.dirs, wd+"/"+fname)
-			continue
+		var fname string
+		for _, file := range fileInfo {
+			fname = file.Name()
+
+			if filepath.Ext(fname) == ".rs" {
+				dot.goFiles = append(dot.goFiles, wd+"/"+fname)
+				continue
+			}
+			if filepath.Ext(fname) == ".yml" {
+				dot.jadeFiles = append(dot.jadeFiles, wd+"/"+fname)
+				continue
+			}
+
+			if file.IsDir() == true && fname[0] != '.' {
+				dot.dirs = append(dot.dirs, wd+"/"+fname)
+				continue
+			}
 		}
 	}
 }
 
+
 func main() {
-	asd := workDir{}
-	asd.getSubdirectory()
-	pp.Println(asd)
+	workFolder := workDir{}
+
+	// wd, err := os.Getwd()
+	// if err != nil {
+	// 	log.Println("Fail Getwd() - %s", err)
+	// 	os.Exit(2)
+	// }
+	// workFolder.dirs = append(workFolder.dirs, wd)
+	workFolder.dirs = append(workFolder.dirs, "../git2-rs")
+
+	var last, cursor int
+	for {
+		last = len(workFolder.dirs)
+		workFolder.parseDir(cursor)
+		if last == cursor { break }
+		cursor = last
+	}
+	pp.Println(workFolder)
 }
