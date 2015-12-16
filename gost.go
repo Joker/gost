439abf51e56f90721bdecf5e396ebdcc27ce8c6b
Joker <nice.way@live.com>
2015-12-16 12:25:20 +0300

    ADD make(), start(), stop()

--- gost.go
+++ gost.go
@@ -1,26 +1,31 @@
 package main
 
 import (
+	"fmt"
+	"io/ioutil"
 	"log"
 	"os"
-	"io/ioutil"
+	"os/exec"
 	"path/filepath"
-	"github.com/k0kubun/pp"
+	"time"
+
+	"github.com/howeyc/fsnotify"
+	// "github.com/k0kubun/pp"
 )
 
+type project struct {
+	dirs      []string
+	goFiles   []string
+	jadeFiles []string
+	makeLock  bool
+	cmd       *exec.Cmd
+}
 
-type workDir 	struct {
-	dirs 		[]string
-	goFiles 	[]string
-	jadeFiles 	[]string
-}	
-
-
-func (dot *workDir) parseDir(sl int) {
+func (dot *project) parseDir(sl int) {
 	for _, wd := range dot.dirs[sl:] {
 		fileInfo, err := ioutil.ReadDir(wd)
 		if err != nil {
-			log.Println("Fail ReadDir() - %s", err)
+			log.Println("Fail ReadDir() - ", err)
 			os.Exit(2)
 		}
 
@@ -28,11 +33,11 @@ func (dot *workDir) parseDir(sl int) {
 		for _, file := range fileInfo {
 			fname = file.Name()
 
-			if filepath.Ext(fname) == ".rs" {
+			if filepath.Ext(fname) == ".go" {
 				dot.goFiles = append(dot.goFiles, wd+"/"+fname)
 				continue
 			}
-			if filepath.Ext(fname) == ".yml" {
+			if filepath.Ext(fname) == ".jade" {
 				dot.jadeFiles = append(dot.jadeFiles, wd+"/"+fname)
 				continue
 			}
@@ -45,24 +50,103 @@ func (dot *workDir) parseDir(sl int) {
 	}
 }
 
+func (dot *project) make() {
+	mk := exec.Command("go", "build", "-o", "a.out")
+	mk.Stdout = os.Stdout
+	mk.Stderr = os.Stderr
+	err := mk.Run()
+	if err != nil {
+		log.Println("Fail run make() - ", err)
+		os.Exit(2)
+	}
+}
+
+func (dot *project) start() {
+	dot.cmd = exec.Command("a.out")
+	dot.cmd.Stdout = os.Stdout
+	dot.cmd.Stderr = os.Stderr
+
+	go dot.cmd.Run()
+}
+
+func (dot *project) stop() {
+	if dot.cmd != nil && dot.cmd.Process != nil {
+		err := dot.cmd.Process.Kill()
+		if err != nil {
+			log.Println("Error cmd.Process.Kill() - ", err)
+		}
+	}
+
+	if rec := recover(); rec != nil {
+		log.Println("Recovered in - ", rec)
+	}
+}
 
 func main() {
-	workFolder := workDir{}
+	pro := project{}
 
-	// wd, err := os.Getwd()
-	// if err != nil {
-	// 	log.Println("Fail Getwd() - %s", err)
-	// 	os.Exit(2)
-	// }
-	// workFolder.dirs = append(workFolder.dirs, wd)
-	workFolder.dirs = append(workFolder.dirs, "../git2-rs")
+	wd, err := os.Getwd()
+	if err != nil {
+		log.Println("Fail Getwd() - ", err)
+		os.Exit(2)
+	}
+	pro.dirs = append(pro.dirs, wd)
+	// pro.dirs = append(pro.dirs, "../test")
+	// os.Chdir(path)
 
 	var last, cursor int
 	for {
-		last = len(workFolder.dirs)
-		workFolder.parseDir(cursor)
-		if last == cursor { break }
+		last = len(pro.dirs)
+		pro.parseDir(cursor)
+		if last == cursor {
+			break
+		}
 		cursor = last
 	}
-	pp.Println(workFolder)
+
+	watcher, err := fsnotify.NewWatcher()
+	if err != nil {
+		log.Println("Fail fsnotify NewWatcher() - ", err)
+		os.Exit(2)
+	}
+
+	go func() {
+		var event string
+		for {
+			select {
+			case ev := <-watcher.Event:
+				if event == fmt.Sprint(time.Now().Unix(), ev) {
+					break
+				}
+				event = fmt.Sprint(time.Now().Unix(), ev)
+				log.Println("-- make")
+				pro.make()
+				log.Println("-- stop")
+				pro.stop()
+				log.Println("-- start")
+				pro.start()
+
+			case err := <-watcher.Error:
+				log.Println("error:", err)
+			}
+		}
+	}()
+
+	for _, dir := range pro.dirs {
+		err = watcher.Watch(dir)
+		if err != nil {
+			log.Println("Fail fsnotify Watch() - ", err)
+			os.Exit(2)
+		}
+	}
+
+	// pro.make()
+
+	quit := make(chan bool)
+	for {
+		select {
+		case <-quit:
+			return
+		}
+	}
 }
